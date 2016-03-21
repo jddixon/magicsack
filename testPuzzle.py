be96cfa581e315e1c831a168dc5770d38f0e0a84
Jim Dixon <jddixon@gmail.com>
2016-03-21 08:29:43 -0700

    add and execute auto8it

--- testPuzzle.py
+++ testPuzzle.py
@@ -2,41 +2,45 @@
 
 # testPuzzle.py
 
-import os, time, unittest
+import os
+import time
+import unittest
 
 from xlattice.crypto import AES_BLOCK_SIZE
-from rnglib         import SimpleRNG
-from magicsack      import *
+from rnglib import SimpleRNG
+from magicsack import *
+
 
 class TestPuzzle (unittest.TestCase):
 
     def setUp(self):
-        self.rng = SimpleRNG( time.time() )
+        self.rng = SimpleRNG(time.time())
+
     def tearDown(self):
         pass
 
     # utility functions #############################################
-    
+
     # actual unit tests #############################################
-   
+
     def testKeyFunc(self):
-        passPhrase  = 'foo' + self.rng.nextFileName(8)
-        salt        = bytes(self.rng.someBytes(16))
-        key         = generateKey(passPhrase, salt, count=20)
+        passPhrase = 'foo' + self.rng.nextFileName(8)
+        salt = bytes(self.rng.someBytes(16))
+        key = generateKey(passPhrase, salt, count=20)
         self.assertTrue(key is not None)
         self.assertEqual(len(key), 2 * AES_BLOCK_SIZE)
 
-    def testPuzzle (self):
-        passPhrase  = 'foo' + self.rng.nextFileName(8)
-        count       = 20    # default is 1000, but this is a test
-        salt        = bytes(self.rng.someBytes(16))
+    def testPuzzle(self):
+        passPhrase = 'foo' + self.rng.nextFileName(8)
+        count = 20    # default is 1000, but this is a test
+        salt = bytes(self.rng.someBytes(16))
 
         # we want this the return an immutable 64-byte value
         puzzle = devisePuzzle(passPhrase, salt, self.rng, count)
-        
+
         ok, key = checkPuzzle(puzzle, passPhrase, salt, count)
         self.assertTrue(ok)
-        self.assertEqual(key == None, False)
+        self.assertEqual(key is None, False)
 
 if __name__ == '__main__':
     unittest.main()
